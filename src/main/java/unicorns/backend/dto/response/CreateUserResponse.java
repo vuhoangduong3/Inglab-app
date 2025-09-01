d55f5c08ac6c4051c7dfcdbef864db876e37205b
Vhd <vuhoangduong3005@gmail.com>
2025-09-01 20:57:41 +0700

    add id to getAllUser api

--- src/main/java/unicorns/backend/dto/response/CreateUserResponse.java
+++ src/main/java/unicorns/backend/dto/response/CreateUserResponse.java
@@ -9,6 +9,7 @@ import java.time.LocalDateTime;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CreateUserResponse {
+    Long id;
     String username;
     String name;
     String email;
