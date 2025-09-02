e452363db1e8e8f42b5e136953813b545ad779ad
Binhnt <nguyenthaibinh.tink32chl@gmail.com>
2025-09-02 11:49:33 +0700

    chore: WIP update SecurityConfig

--- src/main/java/unicorns/backend/config/SecurityConfig.java
+++ src/main/java/unicorns/backend/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("v1/api/auth/login","/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html","/v1/api/auth/refresh-token","/api/exercises/**"
+                                "/swagger-ui.html","/v1/api/auth/refresh-token"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
