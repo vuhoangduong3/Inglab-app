ad7cab264bb7f1d5da23b6288042c5cf3a0ad4e5
Vhd <vuhoangduong3005@gmail.com>
2025-09-15 22:08:32 +0700

    add get Students in Schedule api

--- src/main/java/unicorns/backend/repository/ScheduleRepository.java
+++ src/main/java/unicorns/backend/repository/ScheduleRepository.java
@@ -7,8 +7,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-    List<Schedule> findByClassEntity_Id(Long classId);
-    List<Schedule> findByTeacher_Id(Long teacherId);
     List<Schedule> findByTeacherId(Long teacherId);
     List<Schedule> findByTeacherIdAndDate(Long teacherId, LocalDate date);
 }
